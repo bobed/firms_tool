c157cb4458a4a4843ef660df312896394f6a45ae
bobed <r_henwood@hotmail.com>
2022-06-17 07:31:10 +0100

    tidy up

--- firms_tools.py
+++ firms_tools.py
@@ -4,7 +4,9 @@ import io
 
 from xml.dom.minidom import parse
 
-FIRMS_URL = ""
+FIRMS_DOWNLOAD_URL = ""
+FIRMS_FILE = "MODIS_C6_1_Europe_24h.kml"
+OUTPUT_FILE = "output.xml"
 LOWER_LAT = 45.693156241109214
 UPPER_LAT = 52.44345069237891
 LOWER_LON = 22.43180504054893
@@ -18,32 +20,32 @@ def is_coordinate_in_bounds(raw_coordinate):
     lat = float(coordinates[1])
 
     if lat > LOWER_LAT and lat < UPPER_LAT and lon > LOWER_LON and lon < UPPER_LON:
-        print (coordinate + " in bounds")
+        print(coordinate + " in bounds")
         return True
 
-    print (coordinate + " not in bounds")
+    print(coordinate + " not in bounds")
     return False
 
-def main():
+
+def filter_coordinates():
     # Open XML document using minidom parser
-    with xml.dom.minidom.parse("MODIS_C6_1_Europe_24h.kml") as dom:
+    with xml.dom.minidom.parse(FIRMS_FILE) as dom:
 
         coordinates = dom.documentElement.getElementsByTagName("coordinates")
 
         to_remove = []
-        count = 0
         for coordinate in coordinates:
             coordinate_value = coordinate.firstChild.nodeValue
             in_bounds = is_coordinate_in_bounds(coordinate_value)
-            count = count + 1
             if not in_bounds:
                 to_remove.append(coordinate.parentNode.parentNode)
 
         folder = dom.documentElement.getElementsByTagName("Folder")[0]
         for remove_node in to_remove:
-            folder.removeChild (remove_node)
+            folder.removeChild(remove_node)
 
-        with open("output.kml", "w") as f:
+        with open(OUTPUT_FILE, "w") as f:
             f.write(dom.saveXML(dom.documentElement))
 
-main()
+
+filter_coordinates()
